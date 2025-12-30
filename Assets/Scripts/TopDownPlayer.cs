0706fd71dc5bce3e54c81eb0ff404a771405a543
Adam Loay <adamlo2ay@gmail.com>
2025-12-30 23:28:19 +0200

    top done animation + char done

--- Assets/Scripts/TopDownPlayer.cs
+++ Assets/Scripts/TopDownPlayer.cs
@@ -13,6 +13,7 @@ public class TopDownPlayer : MonoBehaviour
 
     void Start()
     {
+        anim = GetComponent<Animator>();
         spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
@@ -20,40 +21,48 @@ public class TopDownPlayer : MonoBehaviour
     void Update()
     {
         Move();
+        LookAtMouse();
+
+        if(Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            anim.SetTrigger("Shoot");
+        }
+
     }
 
 
     void Move()
     {
         float moveX = Input.GetAxis("Horizontal");
-
-        transform.position += Vector3.right * moveX * speed * Time.deltaTime;
-        if (moveX > 0)
-        {
-            spriteRenderer.flipX = false;
-
-        }
-        else if (moveX < 0)
-        {
-            spriteRenderer.flipX = true;
-        }
-
         float moveY = Input.GetAxis("Vertical");
 
+        if(moveX != 0 || moveY != 0)
+        {
+            anim.SetBool("IsRunning", true);
+        }
+        else
+        {
+            anim.SetBool("IsRunning", false);
+        }
+
+            transform.position += Vector3.right * moveX * speed * Time.deltaTime;
 
 
         transform.position += Vector3.up * moveY * speed * Time.deltaTime;
 
-        if (moveY > 0)
-        {
-            spriteRenderer.flipY = false;
 
-        }
-        else if (moveY < 0)
-        {
-            spriteRenderer.flipY = true;
-        }
 
+    }
+
+    void LookAtMouse()
+    {
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        Vector2 direction = new Vector2( mousePosition.x - transform.position.x, mousePosition.y - transform.position.y );
+
+        float angle = Mathf.Atan2(direction.y , direction.x) * Mathf.Rad2Deg;
+
+        transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle + 90f));
 
     }
 }
